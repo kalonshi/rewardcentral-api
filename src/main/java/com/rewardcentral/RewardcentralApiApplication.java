a0a6fdcc5b5ef524a89fa49d942112ae3da50c90
Patrice LASSEY <splassey@gmail.com>
2022-04-26 14:19:14 +0200

    first commit

---
+++ src/main/java/com/rewardcentral/RewardcentralApiApplication.java
@@ -0,0 +1,13 @@
+package com.rewardcentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardcentralApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardcentralApiApplication.class, args);
+	}
+
+}
